b918906b9339238d29e8b4dd7f69f29a94cb6f74
ConnorFlynn <connor.flynn@student.chaminade.edu>
2021-08-11 14:42:36 -0700

    exercise completed

--- eds-221-day3-comp.R
+++ eds-221-day3-comp.R
@@ -2,7 +2,7 @@
 
 # Packages required
 library(tidyverse)
-
+library(palmerpenguins)
 
 # Refresh boolean operators
 
@@ -93,5 +93,65 @@ switch(species,
 
 
 
+# Afternoon
+
+penguin_summary <- penguins %>% 
+  group_by(species, island) %>% 
+  summarize(mean_bill_depth = mean(bill_depth_mm, na.rm = TRUE),
+  sd_bill_depth = sd(bill_depth_mm, na.rm = TRUE))
+
+penguin_summary
 
 
+penguins_subset <- penguins %>% 
+ filter(island == "Torgersen") %>% 
+  select(species, flipper_length_mm, year)
+
+
+# For loops intro practice
+
+animal <- c("cat", "dog", "dog", "cat", "zebra", "elephant")
+
+for (i in seq_along(animal)) {
+  if(animal[i] == "dog") {
+    print("I love dogs!") 
+  } else {
+    print("This is a different animal")}
+}
+
+
+species <- c("dog", "elephant", "goat", "dog", "dog")
+age_human <- c(3, 8, 4, 6, 12)
+
+
+animal_ages <- vector(mode = "numeric", length = length(species))
+
+for (i in seq_along(species)) {
+  if(species[i] == "dog") {
+    animal_ages <- age_human[i]* 7
+  } else if (species == "elephant" {
+    animal_ages <- age_human[i] * 0.88        
+  } else if (species == "goat") {
+    animal_ages <- age_human[i] * 4.7
+  }
+    animal_ages[i] <- animal_ages
+}
+
+
+# calculate the mean value for each colums in a data frame
+
+mean_mtcars <- vector(mode = "numeric", length = ncol(mtcars))
+
+for (in 1:ncol(mtcars)) {
+  mean_val <- mean(mtcars[[i]]), na.rm = TRUE
+  mean_mtcars[i] <- mean_val
+  }
+
+# Same thing with apply
+apply(x = mtcars, 2, mean, na.rm = TRUE)
+
+
+map_df(.x = mtcars, .f = mean)
+
+
+  
